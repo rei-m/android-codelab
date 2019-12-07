acd2b31b36585d7c4146d06b76ef281fd3bb2dcc
rei-m <star.oceans.rm@gmail.com>
2019-12-08 02:39:18 +0900

    finish android-dagger

--- AndroidDagger/app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ AndroidDagger/app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -26,6 +26,11 @@ open class MyApplication : Application() {
     val appComponent: AppComponent by lazy {
         // Creates an instance of AppComponent using its Factory constructor
         // We pass the applicationContext that will be used as Context in the graph
-        DaggerAppComponent.factory().create(applicationContext)
+//        DaggerAppComponent.factory().create(applicationContext)
+        initializeComponent()
+    }
+
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 }
