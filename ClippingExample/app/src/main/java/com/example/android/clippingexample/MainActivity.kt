a993ad4d8e1d8cb43dd137da936c5429cdc8355e
rei-m <star.oceans.rm@gmail.com>
2019-12-15 02:05:14 +0900

    finish Clipping Canvas Objects

---
+++ ClippingExample/app/src/main/java/com/example/android/clippingexample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.clippingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        setContentView(ClippedView(this))
+    }
+}
