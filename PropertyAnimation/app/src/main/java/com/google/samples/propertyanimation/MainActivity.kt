34b90475b4de9769f8341a449802b419a874ae4c
rei-m <star.oceans.rm@gmail.com>
2019-12-23 02:08:27 +0900

    finish chapter 4 at property-animation

---
+++ PropertyAnimation/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.propertyanimation
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var star: ImageView
+    lateinit var rotateButton: Button
+    lateinit var translateButton: Button
+    lateinit var scaleButton: Button
+    lateinit var fadeButton: Button
+    lateinit var colorizeButton: Button
+    lateinit var showerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        star = findViewById(R.id.star)
+        rotateButton = findViewById(R.id.rotateButton)
+        translateButton = findViewById(R.id.translateButton)
+        scaleButton = findViewById(R.id.scaleButton)
+        fadeButton = findViewById(R.id.fadeButton)
+        colorizeButton = findViewById(R.id.colorizeButton)
+        showerButton = findViewById(R.id.showerButton)
+
+        rotateButton.setOnClickListener {
+            rotater()
+        }
+
+        translateButton.setOnClickListener {
+            translater()
+        }
+
+        scaleButton.setOnClickListener {
+            scaler()
+        }
+
+        fadeButton.setOnClickListener {
+            fader()
+        }
+
+        colorizeButton.setOnClickListener {
+            colorizer()
+        }
+
+        showerButton.setOnClickListener {
+            shower()
+        }
+    }
+
+    private fun rotater() {
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                rotateButton.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                rotateButton.isEnabled = true
+            }
+        })
+        animator.start()
+    }
+
+    private fun translater() {
+    }
+
+    private fun scaler() {
+    }
+
+    private fun fader() {
+    }
+
+    private fun colorizer() {
+    }
+
+    private fun shower() {
+    }
+
+}
